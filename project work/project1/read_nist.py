d7c864b9a5b09cb16aec300bea511eb108199d47
Rebecca Nguyen <rebeccang0610@gmail.com>
2024-09-14 22:06:51 +0200

    C atom file extended to include ionisations stages up to C VI using read_nist.py. Unable to retrieve C VII from NIST...

--- project work/project1/read_nist.py
+++ project work/project1/read_nist.py
@@ -34,11 +34,11 @@ with open(output_filename, 'w') as output:
 for i in range(1, 7): # for every ionization stage 
     filename = f'C_{i}.txt'
     g, chi, chi_ion = read_NIST_species(filename)
-    stage = i
-'''
-    for j in range (len(g)):
-        with open(output_filename, 'a') as output:   
-            # < is for left alignment, 15 is amount of character space
-            append_line = f'\t{chi[j]:<15}\t\t{g[j]}\t{i}\n' # n_stage - 1, due to n_stage = 1 being neutral
+    nstage = i - 1
+
+    for j in range(len(g)):
+        with open(output_filename, 'a') as output: 
+            # < is for left-alignment of text, 18 is for amount of character space
+            append_line = f'\t{chi[j]:<18}\t\t{g[j]}\t{nstage}\n' # nstage - 1, due to nstage = 1 being neutral
             output.write(append_line)
-'''       
+
