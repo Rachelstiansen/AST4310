94fdfe92eb31dc8bebfbe7373c495e303681159e
Rebecca Nguyen <rebeccang0610@gmail.com>
2024-09-14 22:03:14 +0200

    Some bug in read_nist.py. Working on it

--- project work/project1/read_nist.py
+++ project work/project1/read_nist.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def read_NIST_species(input_file):
     """
     Reads data from a text file downloaded from NIST database with
@@ -22,3 +23,22 @@ def read_NIST_species(input_file):
     # clean up missing values
     mask = (g >= 0) & (chi >= 0)
     return g[mask], chi[mask], chi_ion
+
+header = f"#   E (cm^-1)\t\t\tg\tstage" # \t are tabs
+output_filename = 'C_I-VI.txt'
+with open(output_filename, 'w') as output:
+    output.write(header + "\n")
+    output.close
+
+
+for i in range(1, 7): # for every ionization stage 
+    filename = f'C_{i}.txt'
+    g, chi, chi_ion = read_NIST_species(filename)
+    stage = i
+'''
+    for j in range (len(g)):
+        with open(output_filename, 'a') as output:   
+            # < is for left alignment, 15 is amount of character space
+            append_line = f'\t{chi[j]:<15}\t\t{g[j]}\t{i}\n' # n_stage - 1, due to n_stage = 1 being neutral
+            output.write(append_line)
+'''       
