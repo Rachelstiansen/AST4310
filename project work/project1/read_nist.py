07ee9dda2cc73dbc8c697168d3f224bbbc8aa7a1
Rebecca Nguyen <rebeccang0610@gmail.com>
2024-09-15 16:44:09 +0200

    .txt file for C_I-VII is added. read_nist.py is corrected. Other .txt files not in used are deleted

--- project work/project1/read_nist.py
+++ project work/project1/read_nist.py
@@ -24,23 +24,28 @@ def read_NIST_species(input_file):
     mask = (g >= 0) & (chi >= 0)
     return g[mask], chi[mask], chi_ion
 
+# create output file and write a header
 header = f"#   E (cm^-1)\t\t\tg\tstage" # \t are tabs
-output_filename = 'C_I-VI_corrected.txt'
+output_filename = 'C_I-VII.txt'
 with open(output_filename, 'w') as output:
     output.write(header + "\n")
     output.close
 
+# add chi, g and n_stage for each ionisation stage into same output file.
 ion_energy = 0
 for i in range(1, 7): # for every ionization stage 
     filename = f'C_{i}.txt'
     g, chi, chi_ion = read_NIST_species(filename)
-    nstage = i - 1
+    nstage = i - 1 # so that n_stage = 0 is neutral
     chi += ion_energy
 
     for j in range(len(g)):
         with open(output_filename, 'a') as output: 
             # < is for left-alignment of text, 18 is for amount of character space
-            append_line = f'\t{chi[j]:<18}\t\t{g[j]}\t{nstage}\n' # nstage - 1, due to nstage = 1 being neutral
+            append_line = f'\t{chi[j]:<18}\t\t{g[j]}\t{nstage}\n' 
             output.write(append_line)
     ion_energy += chi_ion
 
+# add C VII to output file
+output_file = open(output_filename, 'a')
+output_file.write(f'\t{ion_energy:<18}\t\t1\t6\n')
